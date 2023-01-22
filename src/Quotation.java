a7bfef8cc9c511002be4b5b3bd9dd81a122c08ec
Stickki <palkovlad64@gmail.com>
2023-01-22 20:32:12 +0200

    lab1

--- src/Quotation.java
+++ src/Quotation.java
@@ -1,6 +1,7 @@
 public class Quotation {
   String quote = "Welcome to the party!";
+  String author = "Big Ukranian Boss";
   public void display() {
-    System.out.println(quote);
+    System.out.printf("%s\n%s \n",quote, author);
   }
 }
